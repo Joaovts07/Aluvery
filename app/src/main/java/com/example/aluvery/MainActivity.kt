64ae497c1711a29e52c881d8cabd088e2edc3771
Joao Vitor <joakim.vts@gmail.com>
2023-11-08 20:53:50 -0300

    first commit

---
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.aluvery
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aluvery.ui.theme.AluveryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AluveryTheme {
+                Surface {
+                    MyFirstComposable()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyFirstComposable() {
+    Text(text = "MY First Composeble")
+}
+
+@Preview(
+    name = "PrevieJOe",
+    showSystemUi = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
+
+
+@Preview
+@Composable
+fun MyFirstComposablePreview() {
+    AluveryTheme {
+        Surface {
+            MyFirstComposable()
+
+        }
+    }
+}
