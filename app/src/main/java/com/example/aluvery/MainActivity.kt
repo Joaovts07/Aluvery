568647618406a242a73ffda99b560a8a251ef1a4
Joao Vitor <joakim.vts@gmail.com>
2023-11-24 17:54:51 -0300

    Refactor main activty and create HomeScreen

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -1,50 +1,12 @@
 package com.example.aluvery
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.com.alura.aluvery.extensions.toBrazilianCurrency
-import br.com.alura.aluvery.model.Product
+import com.example.aluvery.ui.screens.HomeScreen
 import br.com.alura.aluvery.ui.theme.AluveryTheme
-import br.com.alura.aluvery.ui.theme.Purple500
-import br.com.alura.aluvery.ui.theme.Teal200
-import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,145 +21,7 @@ class MainActivity : ComponentActivity() {
 fun App() {
     AluveryTheme {
         Surface {
-            Column(
-                Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState()),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                Spacer(Modifier)
-                ProductsSection()
-                ProductsSection()
-                ProductsSection()
-                Spacer(Modifier)
-            }
+            HomeScreen()
         }
     }
-}
-
-@Composable
-fun ProductsSection() {
-    Column {
-        Text(
-            text = "Promoções",
-            Modifier.padding(
-                start = 16.dp,
-                end = 16.dp
-            ),
-            fontSize = 20.sp,
-            fontWeight = FontWeight(400)
-        )
-        Row(
-            Modifier
-                .padding(
-                    top = 8.dp
-                )
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState()),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Spacer(Modifier)
-            ProductItem(
-                Product(
-                    name = "Hamburguer",
-                    price = BigDecimal("12.99"),
-                    image = R.drawable.burger
-                )
-            )
-            ProductItem(
-                Product(
-                    name = "Pizza",
-                    price = BigDecimal("19.99"),
-                    image = R.drawable.pizza
-                )
-            )
-            ProductItem(
-                Product(
-                    name = "Batata frita",
-                    price = BigDecimal("7.99"),
-                    image = R.drawable.fries
-                )
-            )
-            Spacer(Modifier)
-        }
-    }
-}
-
-@Composable
-fun ProductItem(product: Product) {
-    Surface(
-        shape = RoundedCornerShape(15.dp),
-        shadowElevation = 4.dp
-    ) {
-        Column(
-            Modifier
-                .heightIn(250.dp, 300.dp)
-                .width(200.dp)
-        ) {
-            val imageSize = 100.dp
-            Box(
-                modifier = Modifier
-                    .height(imageSize)
-                    .background(
-                        brush = Brush.horizontalGradient(
-                            colors = listOf(
-                                Purple500, Teal200
-                            )
-                        )
-                    )
-                    .fillMaxWidth()
-            ) {
-                Image(
-                    painter = painterResource(id = product.image),
-                    contentDescription = null,
-                    Modifier
-                        .size(imageSize)
-                        .offset(y = imageSize / 2)
-                        .clip(shape = CircleShape)
-                        .align(Alignment.BottomCenter),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Spacer(modifier = Modifier.height(imageSize / 2))
-            Column(Modifier.padding(16.dp)) {
-                Text(
-                    text = product.name,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight(700),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    text = product.price.toBrazilianCurrency(),
-                    Modifier.padding(top = 8.dp),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight(400)
-                )
-            }
-        }
-    }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun AppPreview() {
-    App()
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ProductsSectionPreview() {
-    ProductsSection()
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ProductItemPreview() {
-    ProductItem(
-        Product(
-            name = LoremIpsum(50).values.first(),
-            price = BigDecimal("14.99"),
-            image = R.drawable.ic_launcher_background
-        )
-    )
 }
---
+++ app/src/main/java/com/example/aluvery/ui/screens/HomeScreen.kt
@@ -0,0 +1,43 @@
+package com.example.aluvery.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.alura.aluvery.model.Product
+import com.example.aluvery.R
+import com.example.aluvery.sampledata.sampleProducts
+import com.example.aluvery.ui.components.ProductsSection
+import java.math.BigDecimal
+
+@Composable
+fun HomeScreen() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Spacer(Modifier)
+        ProductsSection("Promoções", sampleProducts)
+        ProductsSection("Doces", listOf(Product(
+            name = "Chocolate",
+            price = BigDecimal("5.99"),
+            image = R.drawable.placeholder
+        )))
+        ProductsSection("Bebidas", sampleProducts)
+        Spacer(Modifier)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
