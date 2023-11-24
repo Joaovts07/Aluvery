84eea24ce11b11f8876d943e8456025177765a40
Joao Vitor <joakim.vts@gmail.com>
2023-11-24 17:44:42 -0300

    add Productions components

---
+++ app/src/main/java/com/example/aluvery/ui/components/ProductsSection.kt
@@ -0,0 +1,53 @@
+package com.example.aluvery.ui.components
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.alura.aluvery.model.Product
+import com.example.aluvery.sampledata.sampleProducts
+
+@Composable
+fun ProductsSection(
+    title: String,
+    products: List<Product>
+) {
+    Column {
+        Text(
+            text = title,
+            Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            Modifier
+                .padding(
+                    top = 8.dp
+                )
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(Modifier)
+            for (p in products) {
+                ProductItem(product = p)
+            }
+            Spacer(Modifier)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductsSectionPreview() {
+    ProductsSection("Promoções", products = sampleProducts)
+}
