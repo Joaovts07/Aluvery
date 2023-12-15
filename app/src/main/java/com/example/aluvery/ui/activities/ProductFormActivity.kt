ca40da765bcee55a72a2b8b73a4a092759349822
Joao Vitor <joakim.vts@gmail.com>
2023-12-15 17:54:11 -0300

    add Async img when user typing url

--- app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/ProductFormActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
@@ -23,9 +22,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.aluvery.R
 import com.example.aluvery.model.Product
 import com.example.aluvery.ui.theme.AluveryTheme
 import java.lang.NumberFormatException
@@ -47,14 +50,30 @@ class ProductFormActivity: ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductFormScreen() {
-    Column(Modifier.fillMaxSize()
-        .padding(16.dp),
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
         Text(text = "Criando o produto", Modifier.fillMaxWidth(), fontSize = 28.sp)
         var url by remember {
             mutableStateOf("")
         }
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+
         TextField(value = url, onValueChange = {
             url = it
         }, Modifier.fillMaxWidth(), label = {
@@ -81,7 +100,10 @@ fun ProductFormScreen() {
         }
         TextField(value = description, onValueChange = {
             description = it
-        }, Modifier.fillMaxWidth().heightIn( min = 100.dp), label = {
+        },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp), label = {
             Text(text = "Descrição")
         })
         Button(onClick = {
