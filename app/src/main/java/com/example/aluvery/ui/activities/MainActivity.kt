5c32eb7cd78513579463a5be4735909ccc086014
Joao Vitor <joakim.vts@gmail.com>
2023-12-24 17:26:50 -0300

    add FAB and click event

--- app/src/main/java/com/example/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/ui/activities/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.example.aluvery.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.aluvery.ui.screens.HomeScreen
 import com.example.aluvery.ui.theme.AluveryTheme
 import com.example.aluvery.sampledata.sampleSections
@@ -13,16 +23,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            App(onFabClick = {
+                startActivity(Intent(this, ProductFormActivity::class.java))
+            })
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun App() {
+fun App(onFabClick: () -> Unit) {
     AluveryTheme {
         Surface {
-            HomeScreen(sampleSections)
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = { onFabClick() }) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    HomeScreen(sampleSections)
+
+                }
+
+            }
         }
     }
 }
