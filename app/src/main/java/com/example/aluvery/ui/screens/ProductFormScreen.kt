b5dfd571687135b8f35646354bca72995e8a8631
Joao Vitor <joakim.vts@gmail.com>
2024-01-25 11:54:58 -0300

    Refactor create a screen for ProductFormScreen

---
+++ app/src/main/java/com/example/aluvery/ui/screens/ProductFormScreen.kt
@@ -0,0 +1,162 @@
+package com.example.aluvery.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.aluvery.R
+import com.example.aluvery.model.Product
+import com.example.aluvery.ui.components.ColumnTextFieldError
+import java.lang.NumberFormatException
+import java.math.BigDecimal
+
+class ProductFormScreenUIState(
+    val onSaveClick: (Product) -> Unit ={},
+) {
+    fun isNotBlank(url: String): Boolean = url.isNotBlank()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductFormScreen(
+    state: ProductFormScreenUIState = ProductFormScreenUIState()
+) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Spacer(modifier = Modifier)
+        Text(text = "Criando o produto", Modifier.fillMaxWidth(), fontSize = 28.sp)
+        var url by remember {
+            mutableStateOf("")
+        }
+        if (state.isNotBlank(url)) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+
+        TextField(value = url, onValueChange = {
+            url = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Url da imagem")
+        },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            )
+        )
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        }, Modifier.fillMaxWidth(), label = {
+            Text(text = "Nome")
+        },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            )
+        )
+        var price by remember {
+            mutableStateOf("")
+        }
+        var isPriceError by remember {
+            mutableStateOf(false)
+        }
+
+        ColumnTextFieldError(price,
+            isPriceError,
+            onTextChange = {
+                /*isPriceError = try {
+                    BigDecimal(it)
+                    false
+                } catch (e: IllegalArgumentException) {
+                    it.isNotEmpty()
+                }
+                price = it*/
+
+            })
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(value = description, onValueChange = {
+            description = it
+        },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(min = 100.dp), label = {
+                Text(text = "Descrição")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Sentences
+            )
+        )
+        Button(onClick = {
+            val convertedPrice = try {
+                BigDecimal(price)
+            } catch (e: NumberFormatException) {
+                BigDecimal.ZERO
+            }
+            val product = Product(
+                name = name,
+                image = url,
+                price = convertedPrice,
+                description = description
+            )
+            state.onSaveClick(product)
+        },
+            Modifier.fillMaxWidth(), shape = RectangleShape,
+        ) {
+            Text(text = "Salvar")
+        }
+        Spacer(modifier = Modifier)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    ProductFormScreen()
+}
